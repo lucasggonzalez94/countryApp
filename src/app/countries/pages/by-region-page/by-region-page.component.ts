b6fcb7c523e5ba6babe4361fe3f9e99ebbb5e29d
lucasggonzalez94 <lucasggonzalez94@outlook.com>
2023-10-24 13:12:51 -0300

    definir botones regiones

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country.interface';
 import { CountriesService } from '../../services/countries.service';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania'
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -11,13 +13,19 @@ import { CountriesService } from '../../services/countries.service';
 })
 export class ByRegionPageComponent {
   public countries: Country[] = [];
+  public isLoading: boolean = false;
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion?: Region;
 
   constructor( private countriesService: CountriesService ) {}
 
-  searchByRegion(term: string): void {
+  searchByRegion(term: Region): void {
+    this.selectedRegion = term;
+    this.isLoading = true;
     this.countriesService.searchRegion(term)
       .subscribe( (countries: Country[]) => {
         this.countries = countries;
+        this.isLoading = false;
       });
   }
 }
